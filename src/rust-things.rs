bb9c180f832798a968704843592e29c5e6ff7b13
Simon Gardling <titaniumtown@gmail.com>
2021-03-08 12:51:52 -0500

    more stuff

--- src/rust-things.rs
+++ src/rust-things.rs
@@ -1,5 +1,19 @@
 mod threecubes;
 
 fn main() {
-    threecubes::basicthreecubes(1000, 999);
+    let argument = "--basic-three-cubes";
+    #[allow(unused_mut)]
+    let mut output; // Output variable
+    #[allow(unused_mut)]
+    let mut smallestcube;
+    if argument == "--basic-three-cubes" {
+        output = threecubes::basicthreecubes(1000, 69420);
+        println!("List of cubes: {:?}", output);
+        smallestcube = threecubes::getsmallestcube(output);
+        println!("Smallest cube: {:?}", smallestcube);
+        
+    } else {
+        println!("Error: argument invalid");
+    }
+    
 }
--- src/threecubes.rs
+++ src/threecubes.rs
@@ -1,8 +1,9 @@
-pub fn basicthreecubes(maxrange: i64, target: i64) {
-    let mut xtmp; let mut ytmp; let mut ztmp; // tmp vars for caching expontents
-    let mut solnum = 0; // # of solutions found
+pub fn basicthreecubes(maxrange: i64, target: i64) -> Vec<[i64; 3]> {
     println!("max range: {}\ntarget: {}\ntotal possible numbers: {}", maxrange, target, i64::pow(2*maxrange, 3));
-    
+
+    let mut xtmp; let mut ytmp; // tmp vars for caching expontents
+    let mut solnum = 0; // # of solutions found
+    let mut solutions: Vec<[i64; 3]> = Vec::new(); // list of solutions found
     let mut expontents = Vec::new(); // list that holds cached exponents
 
     // cache exponents to be called later
@@ -19,13 +20,28 @@ pub fn basicthreecubes(maxrange: i64, target: i64) {
         for y in -maxrange..maxrange {
             ytmp = expontents[(y+maxrange) as usize];
             for z in -maxrange..maxrange {
-                ztmp = expontents[(z+maxrange) as usize];
-                if xtmp+ytmp+ztmp == target {
+                if xtmp+ytmp+expontents[(z+maxrange) as usize] == target {
                     println!("found: {}, {}, {}", x, y, z);
                     solnum += 1;
+                    solutions.push([x,y,z])
                 }
             }
         }
     }
     println!("total found: {}", solnum);
+    return solutions;
+}
+
+pub fn getsmallestcube(inlist: Vec<[i64; 3]>) -> [i64; 3] {
+    let mut output: [i64; 3] = [0, 0, 0];
+    let mut smallestsum = std::i64::MAX;
+    let mut procsum;
+    for ele in inlist {
+        procsum = ele[0].abs()+ele[1].abs()+ele[2].abs();
+        if smallestsum > procsum {
+            output = ele;
+            smallestsum = procsum;
+        }
+    }
+    return output;
 }
